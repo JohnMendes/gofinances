7b53f7175cb3073e80876430c9f163bdb2fbe51e
john willian de jesus mendes <john.willian.jm@gmail.com>
2021-09-22 21:00:47 -0300

    Gofinences
    Criando Aplicação

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text} from 'react-native'
+
+import {Container, Title} from './styles'
+
+export function DashBoard() {
+
+  return (
+    <Container >
+      <Title>Bashboard</Title>
+    </Container>
+  )
+}
+
+
