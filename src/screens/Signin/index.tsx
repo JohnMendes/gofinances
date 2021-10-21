8022cf03da238a4a875505b2b8afc8189e4b7d35
john willian de jesus mendes <john.willian.jm@gmail.com>
2021-10-21 07:52:08 -0300

    Ultima Aula

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {useContext} from "react";
+import { RFValue } from "react-native-responsive-fontsize";
+import {Alert} from "react-native"
 
+import AppleSvg from "../../assets/apple.svg";
+import GoogleSvg from "../../assets/google.svg";
+import LogoSvg from "../../assets/logo.svg";
 
-import {Container} from './styles'
+import {useAuth} from '../../Hooks/auth'
 
-export function Signin(){
+import {SingnInSocialButton} from '../../components/SingInSocialBotton'
+
+import { Container, Header, TitleWrapper , Title, SignInTitle, Footer, FooterWrapper} from "./styles";
+
+export function Signin() {
+
+  const {SignInWithGoogle} = useAuth();
+  
+  async function handleSignInWithGoogle(){
+    try {
+      await SignInWithGoogle(); 
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possivel conectar a conta Google')
+    }
+  }
 
   return (
     <Container>
-      
+      <Header>
+        <TitleWrapper>
+          <LogoSvg width={RFValue(120)} height={RFValue(68)} />          
+        </TitleWrapper>
+        <Title>Controle suas {'\n'} finanças de forma {'\n'} muito simples</Title>
+        <SignInTitle>Faça seu login com {'\n'} uma das contas abaixo</SignInTitle>
+      </Header>
+
+      <Footer >
+        <FooterWrapper>
+          <SingnInSocialButton title="Entrar com Google" svg={GoogleSvg} onPress={handleSignInWithGoogle} />
+          <SingnInSocialButton title="Entrar com Apple" svg={AppleSvg} />  
+        </FooterWrapper>
+      </Footer>
     </Container>
-  )
+  );
 }
